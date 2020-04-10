08953d6ac1542585e1d0e7562d9b01420979c72b
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-10 10:58:12 -0600

    Refactor test

--- spec/features/user_can_get_direction_for_closest_fueling_station_spec.rb
+++ spec/features/user_can_get_direction_for_closest_fueling_station_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "As a user" , type: :feature do 
+RSpec.describe 'As a user' , type: :feature do 
 
-  describe "When I select Turing and search for nearest station" do 
+  describe 'When I select Turing and search for nearest station' do 
 
-    it "it shows me the address of the nearest station and also the directions" do
+    it 'it shows me the address of the nearest station and also the directions' do
       visit '/'
       select 'Turing', from: :location
       click_on 'Find Nearest Station'
       expect(current_path).to eq('/search')
 
       within('.station') do
-        expect(first(".name").text).to_not be_empty
-        expect(first(".address").text).to_not be_empty
-        expect(first(".fuel_type").text).to_not be_empty
-        expect(first(".access_times").text).to_not be_empty
+        expect(first('.name').text).to_not be_empty
+        expect(first('.address').text).to_not be_empty
+        expect(first('.fuel_type').text).to_not be_empty
+        expect(first('.access_times').text).to_not be_empty
       end
 
       within('.direction') do
-        expect(first(".distance").text).to_not be_empty
-        expect(first(".travel_time").text).to_not be_empty
-        expect(first(".direction_instructions").text).to_not be_empty
+        expect(first('.distance').text).to_not be_empty
+        expect(first('.travel_time').text).to_not be_empty
+        expect(first('.direction_instructions').text).to_not be_empty
       end
     end
 

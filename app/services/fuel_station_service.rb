a33e1c208ff81d311a161de66e89430aed4009cc
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-10 10:46:33 -0600

    Refactor the services

--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -1,13 +1,16 @@
 class FuelStationService
 
   def self.nearest_station_information(location)
-    conn1 = Faraday.new "https://developer.nrel.gov/api/alt-fuel-stations/v1" do |conn|
-      conn.params['api_key'] = ENV['DEV_NETWORK_API_KEY']
-    end
-    response = conn1.get('nearest.json?') do |req|
+    response = conn.get('nearest.json?') do |req|
       req.params['location'] = location
       req.params['fuel_type'] = 'ELEC'
     end
     return response
+  end
+
+  def self.conn 
+    Faraday.new "https://developer.nrel.gov/api/alt-fuel-stations/v1" do |conn|
+      conn.params['api_key'] = ENV['DEV_NETWORK_API_KEY']
+    end
   end
 end
--- app/services/google_direction_service.rb
+++ app/services/google_direction_service.rb
@@ -1,14 +1,17 @@
 class GoogleDirectionService
 
   def self.direction_to_location(origin, destination)
-    conn = Faraday.new "https://maps.googleapis.com/maps/api" do |conn|
-      conn.params['key'] = ENV['GOOGLE_MAP_API_KEY']
-    end
     response = conn.get('directions/json?') do |req|
       req.params['origin'] = origin
       req.params['destination'] = destination
     end
     return response
+  end
+
+  def self.conn
+    Faraday.new "https://maps.googleapis.com/maps/api" do |conn|
+      conn.params['key'] = ENV['GOOGLE_MAP_API_KEY']
+    end
   end
 
 end

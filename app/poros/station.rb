4ff1ea2acc62d4466551087b5a1a6cbb3b83cc98
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-10 09:28:30 -0600

    Add station poro

---
+++ app/poros/station.rb
@@ -0,0 +1,10 @@
+class Station
+  
+  def initialize(info) 
+    @name = info[:name ]
+    @address = info[:address]
+    @fuel_type = info[:fuel_type]
+    @access_times = info[:access_times]
+  end 
+  
+end
